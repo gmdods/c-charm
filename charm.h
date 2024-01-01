188e01c539bea5bd89d9221edf2224f0407af362
gmd01bit <gmd01bit@gmail.com>
2024-01-01 16:33:20 +0200

    :sparkles: Adding char names and more types

--- charm.h
+++ charm.h
@@ -6,21 +6,26 @@
 #define CONCAT(lhs, rhs) CAT(lhs, rhs)
 #define STRING(macro) #macro
 
-#define COMMA ,
+#define EMPTY()
+#define COMMA() ,
 #define ID(c) c
 #define LPAREN (
 #define RPAREN )
 #define LPAREN_S(x) ( (x)
 #define RPAREN_S(x) (x) )
-#define EXPAND(x) (x)
+#define EXPAND(x) x
+#define SINGLE(x, y) (y)
 
-#define cases(F) F(ID, : case)
+#define cases(F) F(SINGLE, : case)
 #define fallthrough ((void) 0)
 
-#define anyof(var, F) EXPAND(LPAREN F((var) == RPAREN_S, | LPAREN_S))
-#define noneof(var, F) EXPAND(LPAREN F((var) != RPAREN_S, &LPAREN_S))
+#define anyof(var, F) (EXPAND(LPAREN F((var) == RPAREN_S SINGLE, | LPAREN_S)))
+#define noneof(var, F) (EXPAND(LPAREN F((var) != RPAREN_S SINGLE, &LPAREN_S)))
 
-#define many(F) F(ID, COMMA)
+#define toenum(n, x) n = x
+#define enumerate(F) EXPAND(EMPTY F(() toenum, COMMA))
+
+#define many(F) EXPAND(EMPTY F((), COMMA))
 
 #endif // !CHARM_MACROS_H
 
@@ -33,44 +38,77 @@ typedef unsigned char char8_t;
 #endif // !char8_t
 
 #define lowers(X, S) \
-	X('a') \
-	S X('b') S X('c') S X('d') S X('e') S X('f') S X('g') S X('h') \
-	    S X('i') S X('j') S X('k') S X('l') S X('m') S X('n') S X('o') \
-		S X('p') S X('q') S X('r') S X('s') S X('t') S X('u') S X('v') \
-		    S X('w') S X('x') S X('y') S X('z')
+	X(LOWER_A, 'a') \
+	S X(LOWER_B, 'b') S X(LOWER_C, 'c') S X(LOWER_D, 'd') \
+	    S X(LOWER_E, 'e') S X(LOWER_F, 'f') S X(LOWER_G, 'g') \
+		S X(LOWER_H, 'h') S X(LOWER_I, 'i') S X(LOWER_J, 'j') \
+		    S X(LOWER_K, 'k') S X(LOWER_L, 'l') S X(LOWER_M, 'm') \
+			S X(LOWER_N, 'n') S X(LOWER_O, 'o') S X(LOWER_P, 'p') \
+			    S X(LOWER_Q, 'q') S X(LOWER_R, 'r') \
+				S X(LOWER_S, 's') S X(LOWER_T, 't') \
+				    S X(LOWER_U, 'u') S X(LOWER_V, 'v') \
+					S X(LOWER_W, 'w') S X(LOWER_X, 'x') \
+					    S X(LOWER_Y, 'y') \
+						S X(LOWER_Z, 'z')
 
 #define uppers(X, S) \
-	X('A') \
-	S X('B') S X('C') S X('D') S X('E') S X('F') S X('G') S X('H') \
-	    S X('I') S X('J') S X('K') S X('L') S X('M') S X('N') S X('O') \
-		S X('P') S X('Q') S X('R') S X('S') S X('T') S X('U') S X('V') \
-		    S X('W') S X('X') S X('Y') S X('Z')
+	X(UPPER_A, 'A') \
+	S X(UPPER_B, 'B') S X(UPPER_C, 'C') S X(UPPER_D, 'D') \
+	    S X(UPPER_E, 'E') S X(UPPER_F, 'F') S X(UPPER_G, 'G') \
+		S X(UPPER_H, 'H') S X(UPPER_I, 'I') S X(UPPER_J, 'J') \
+		    S X(UPPER_K, 'K') S X(UPPER_L, 'L') S X(UPPER_M, 'M') \
+			S X(UPPER_N, 'N') S X(UPPER_O, 'O') S X(UPPER_P, 'P') \
+			    S X(UPPER_Q, 'Q') S X(UPPER_R, 'R') \
+				S X(UPPER_S, 'S') S X(UPPER_T, 'T') \
+				    S X(UPPER_U, 'U') S X(UPPER_V, 'V') \
+					S X(UPPER_W, 'W') S X(UPPER_X, 'X') \
+					    S X(UPPER_Y, 'Y') \
+						S X(UPPER_Z, 'Z')
 
 #define alphas(X, S) lowers(X, S) S uppers(X, S)
 
 #define digits(X, S) \
-	X('0') \
-	S X('1') S X('2') S X('3') S X('4') S X('5') S X('6') S X('7') \
-	    S X('8') S X('9')
+	X(DIGIT_0, '0') \
+	S X(DIGIT_1, '1') S X(DIGIT_2, '2') S X(DIGIT_3, '3') \
+	    S X(DIGIT_4, '4') S X(DIGIT_5, '5') S X(DIGIT_6, '6') \
+		S X(DIGIT_7, '7') S X(DIGIT_8, '8') S X(DIGIT_9, '9')
 
 #define alphadigits(X, S) alphas(X, S) S digits(X, S)
 
-#define blanks(X, S) X(' ') S X('\t')
+#define blanks(X, S) X(SPACE, ' ') S X(TAB, '\t')
 
-#define linespaces(X, S) blanks(X, S) S X('\r') S X('\f') S X('\v')
-#define spaces(X, S) linespaces(X, S) S X('\n')
+#define linespaces(X, S) \
+	blanks(X, S) S X(CR, '\r') S X(FEED, '\f') S X(VTAB, '\v')
+#define spaces(X, S) linespaces(X, S) S X(LF, '\n')
+
+#define openbrackets(X, S) \
+	X(OPEN_PAREN, '(') S X(OPEN_SQUARE, '[') S X(OPEN_BRACES, '{')
+#define closebrackets(X, S) \
+	X(CLOSE_PAREN, ')') S X(CLOSE_SQUARE, ']') S X(CLOSE_BRACES, '}')
+#define brackets(X, S) openbrackets(X, S) S closebrackets(X, S)
+
+#define points(X, S) \
+	X(SEMICOLON, ';') S X(COLON, ':') S X(PERIOD, '.') S X(COMMA, ',')
+
+#define quotes(X, S) X(BACKTICK, '`') S X(QUOTE, '\'') S X(DOUBLE_QUOTE, '\"')
+
+#define mathematics(X, S) \
+	X(TILDE, '~') \
+	S X(EXCLAMATION, '!') S X(PERCENT, '%') S X(CARAT, '^') \
+	    S X(AMPERSAND, '&') S X(ASTERISK, '*') S X(UNDERSCORE, '_') \
+		S X(HYPHEN, '-') S X(EQUAL, '=') S X(PLUS, '+') S X(PIPE, '|') \
+		    S X(LEFT_ANGLE, '<') S X(RIGHT_ANGLE, '>') S X(SLASH, '/')
 
 #define punct(X, S) \
-	X('`') \
-	S X('~') S X('!') S X('@') S X('#') S X('$') S X('%') S X('^') \
-	    S X('&') S X('*') S X('(') S X(')') S X('_') S X('-') S X('=') \
-		S X('+') S X('[') S X('{') S X(']') S X('}') S X('\\') \
-		    S X('|') S X(';') S X(':') S X('\'') S X('\"') S X(',') \
-			S X('<') S X('.') S X('>') S X('/') S X('?')
+	brackets(X, S) S points(X, S) \
+	S quotes(X, S) \
+	S mathematics(X, S) \
+	S X(DOLLAR, '$') S X(BACKSLASH, '\\') S X(AT, '@') S X(HASH, '#') \
+	    S X(QUESTION, '?')
 
 #define graphs(X, S) alphadigits(X, S) S punct(X, S)
 
-#define idents(X, S) alphas(X, S) S X('_')
+#define idents(X, S) alphas(X, S) S X(UNDERSCORE, '_')
 
 #define lexemes(X, S) idents(X, S) S digits(X, S)
 
--- test/unittest.c
+++ test/unittest.c
@@ -31,22 +31,33 @@ unittest("anyof") {
 unittest("lexer") {
 
 	const char8_t * str = (const char8_t *) "let var = 30;\n"
-						"let letter = 'a';";
-	enum tags { LET, IDENT, EQUAL, DECIMAL, CHAR, SEMICOLON };
+						"let letter = 'a';\n"
+						"var + (letter - 'a') * 3";
+	enum tags {
+		LET,
+		IDENT,
+		DECIMAL,
+		enumerate(punct),
+	};
 	const struct token {
 		enum tags tag;
 		const char8_t * ptr;
 		size_t length;
 	} expected[] = {
-	    {LET, str + 0, 3},	      {IDENT, str + 4, 3},
-	    {EQUAL, str + 8, 1},      {DECIMAL, str + 10, 2},
-	    {SEMICOLON, str + 12, 1}, {LET, str + 14, 3},
-	    {IDENT, str + 18, 6},     {EQUAL, str + 25, 1},
-	    {CHAR, str + 28, 1},      {SEMICOLON, str + 30, 1},
+	    {LET, str + 0, 3},		{IDENT, str + 4, 3},
+	    {EQUAL, str + 8, 1},	{DECIMAL, str + 10, 2},
+	    {SEMICOLON, str + 12, 1},	{LET, str + 14, 3},
+	    {IDENT, str + 18, 6},	{EQUAL, str + 25, 1},
+	    {QUOTE, str + 28, 1},	{SEMICOLON, str + 30, 1},
+	    {IDENT, str + 32, 3},	{PLUS, str + 36, 1},
+	    {OPEN_PAREN, str + 38, 1},	{IDENT, str + 39, 6},
+	    {HYPHEN, str + 46, 1},	{QUOTE, str + 49, 1},
+	    {CLOSE_PAREN, str + 51, 1}, {ASTERISK, str + 53, 1},
+	    {DECIMAL, str + 55, 1},
 	};
 #define ensure_token(a, b) ensure(memcmp(&(a), &(b), sizeof(struct token)) == 0)
 
-	char8_t c = '\0';
+	char8_t c = '\0', q = '\0';
 	size_t index = 0;
 	size_t linenum = 0, lineindex = 0;
 	for (const char8_t *it = str, *ptr = 0;;)
@@ -56,21 +67,23 @@ unittest("lexer") {
 			++it, ++linenum;
 			lineindex = it - str;
 			break;
-		case '=':
-			ensure_token(expected[index++],
-				     ((struct token){EQUAL, it, 1}));
+		case '#': // Comment
+			while ((c = *(++it)) && (c != '\n'))
+				;
 			++it;
 			break;
-		case ';':
+		case cases(mathematics):
+		case cases(points):
+		case cases(brackets):
 			ensure_token(expected[index++],
-				     ((struct token){SEMICOLON, it, 1}));
+				     ((struct token){c, it, 1}));
 			++it;
 			break;
-		case '\'':
-			for (ptr = ++it; (c = *it) && (c != '\''); ++it)
+		case cases(quotes):
+			for (ptr = ++it, q = c; (c = *it) && (c != q); ++it)
 				;
 			ensure_token(expected[index++],
-				     ((struct token){CHAR, ptr, it - ptr}));
+				     ((struct token){q, ptr, it - ptr}));
 			++it;
 			break;
 		case cases(alphas):
@@ -95,5 +108,5 @@ unittest("lexer") {
 		case '\0': goto token_ret;
 		}
 token_ret:
-	ensure(linenum == 1), ensure(lineindex == 14);
+	ensure(linenum == 2), ensure(lineindex == 32);
 }
